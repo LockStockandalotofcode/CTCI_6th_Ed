037bfa225958fb36ad5771c1d7e328eb93b8060f
LockStockandalotofcode <shivanie0101@gmail.com>
2026-01-30 00:10:25 +0530

    1.2 Check Permutation

---
+++ ch_01/p_02_valid_anagram.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # Length check
+        if len(s) != len(t):
+            return False
+            
+        # Dictionary to keep track of characters encountered
+        # key = character, value = frequency of character
+        track = {}
+
+        for char in s:
+            # If char isn't there in track dict, start at 0 and add 1
+            track[char] = track.get(char, 0) + 1
+
+        for char in t:
+            if char not in track or track[char] == 0:
+                return False
+            else:
+                track[char] -= 1
+
+        return True
+
+# Example usage:
+# sol = Solution()
+# print(sol.isAnagram("anagram", "nagaram"))
