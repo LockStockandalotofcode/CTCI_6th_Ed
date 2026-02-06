07a8ff508f413a122b8636b922787ed9fa12a8b6
SHIVANIE <114804804+LockStockandalotofcode@users.noreply.github.com>
2026-02-06 23:19:29 +0530

    6 ms (46.49%) 20.4 MB (55.26%) 02-06-2026 0283-move-zeroes Easy

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        current_freq = 0 # current frequency of zeroes
+        n = len(nums)
+        for i in range(n):
+            if nums[i] == 0:
+                current_freq += 1
+            else:
+                nums[i - current_freq] = nums[i]
+        
+        for i in range(n-current_freq, n):
+            nums[i] = 0
