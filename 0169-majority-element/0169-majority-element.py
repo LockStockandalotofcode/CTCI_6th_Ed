24696af1e6e31d9c63f8ec714a59e91dbd2d860d
SHIVANIE <114804804+LockStockandalotofcode@users.noreply.github.com>
2026-02-06 22:38:30 +0530

    7 ms (57.68%) 21.1 MB (48.35%) 02-06-2026 0169-majority-element Easy

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # Hashmap(dict) approach O(n) time and O(n) space
+        tracker = {}
+        for num in nums:
+            tracker[num] = tracker.get(num, 0) + 1
+
+        majority_element, max_count = 0, 0
+        for num, count in tracker.items():
+            if count > max_count:
+                max_count = count
+                majority_element = num
+
+        return majority_element
