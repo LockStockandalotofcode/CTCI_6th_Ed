ee973da32a9c68e4da8904f120a81c5304aef3ac
SHIVANIE <114804804+LockStockandalotofcode@users.noreply.github.com>
2026-02-14 22:56:50 +0530

    Time: 0 ms (100%) , Space: 20.5 MB (56.67%)  - 0110-balanced-binary-tree

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree-02-14-2026-22-56-49.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def helper(node):
+            if not node: return 0
+
+            left_height = helper(node.left)
+            if left_height == -1: return -1
+            right_height = helper(node.right)
+            if right_height == -1: return -1
+
+            if abs(left_height - right_height) > 1:
+                return -1
+            else:
+                return 1 + max(left_height, right_height)
+
+        balanced_flag = helper(root)
+        if balanced_flag == -1:
+            return False
+        else:
+            return True
