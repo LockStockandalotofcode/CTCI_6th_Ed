9876920495b9453b5d386caecbf241a5b804e57e
SHIVANIE <114804804+LockStockandalotofcode@users.noreply.github.com>
2026-02-17 19:43:39 +0530

    Time: N/A (0%) , Space: N/A (0%)  - 0278-first-bad-version

---
+++ 0278-first-bad-version/0278-first-bad-version-02-17-2026-19-43-39.py
@@ -0,0 +1,17 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left, right = 1, n
+
+        while left < right:
+            mid = left + (left + right) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        # when we reach at left == right, we've reached the first bad version
+            
+        return left
