2a39002eef2d6e9da4f91cebf9eb052a6e33bcb0
SHIVANIE <114804804+LockStockandalotofcode@users.noreply.github.com>
2026-03-05 11:40:01 +0530

    Time: 4 ms (80.74%) , Space: 19.4 MB (52.02%)  - 0009-palindrome-number

---
+++ 0009-palindrome-number/0009-palindrome-number-03-05-2026-11-40-00.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x % 10 == 0 and x != 0): # any number with trailing zeroes is not a palindrome, since it must start with 0 as well
+            return False
+        
+        revertedNum = 0
+        while x > revertedNum:
+            revertedNum = (revertedNum * 10) + (x % 10)
+            x //= 10
+
+        return x == revertedNum or x == revertedNum // 10
