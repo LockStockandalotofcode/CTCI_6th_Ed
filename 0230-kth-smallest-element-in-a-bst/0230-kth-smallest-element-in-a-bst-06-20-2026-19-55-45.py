53e47b3e89d1e9e6439f320c9a0f5b7dc345f88e
SHIVANIE <114804804+LockStockandalotofcode@users.noreply.github.com>
2026-06-20 19:55:46 +0530

    Time: 3 ms (30.58%) , Space: 23 MB (10.99%)  - 0230-kth-smallest-element-in-a-bst

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst-06-20-2026-19-55-45.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        sort_map = {}
+        
+        rank = 1
+        # BST to map all node values to their rank
+        def sort_bst(node: Optional[TreeNode]):
+            nonlocal rank
+
+            if node.left:
+                sort_bst(node.left)
+            
+            sort_map[rank] = node.val
+            rank += 1
+
+            if node.right:
+                sort_bst(node.right)
+            
+        sort_bst(root)
+            
+        res = sort_map.get(k)
+        return res
