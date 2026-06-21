c47aa4aaa2d2edd7c6af81397be78f59042958a4
SHIVANIE <114804804+LockStockandalotofcode@users.noreply.github.com>
2026-06-21 14:08:19 +0530

    Time: 0 ms (100%) , Space: 19.3 MB (39.96%)  - 0017-letter-combinations-of-a-phone-number

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number-06-21-2026-14-08-18.py
@@ -0,0 +1,25 @@
+import itertools
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        # BASE CASE
+        if not digits:
+            return []
+        
+        # HASH MAP OF CONFIGURATIONS
+        phone_map = {
+            "2" : "abc", "3" : "def","4" : "ghi","5" : "jkl","6" : "mno","7" : "pqrs","8" : "tuv","9" : "wxyz"
+        }
+
+        # PYTHONIC WAY
+
+        # get letter groups corresponding to each digit
+        letter_grps = [phone_map[d] for d in digits]
+
+        # generate combinations with cartesian product logic by itertools.product
+        # the combinations generated are array of character strings 
+        combinations = itertools.product(*letter_grps)
+        # * : unpacking operator allows use of dynamic list as argument in above function
+        
+        # the combinations generated are array of character strings 
+        # make proper strings and make the final list
+        return ["".join(c) for c in combinations]
